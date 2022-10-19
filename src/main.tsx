d411ecbc530ca35141ba6abf84098222c41c1e66
lhw <1097019865@qq.com>
2022-10-19 19:36:12 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
+import routes from "@/router";
+let router = createBrowserRouter(routes)
 let root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <RouterProvider router={router}>
+
+        </RouterProvider>
     </React.StrictMode>
 )
